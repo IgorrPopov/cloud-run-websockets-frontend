f7ba1b312a7f66de86fe1d2418d38843a3205ef0
Igor Popov <ivpoov@gmail.com>
2022-01-21 11:52:27 +0200

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,52 @@
+import { v4 as uuidv4 } from 'uuid';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { Component } from '@angular/core';
+import { ChatService } from './chat.service';
 
-import { AppComponent } from './app.component';
+const config: any = { 
+  url: 'https://test-app-kbsq3z36sa-uc.a.run.app/', 
+  options: { 
+    query: { 
+      userId: uuidv4(),
+    },
+  } 
+};
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+class AppComponent {
+  userId = config.options.query.userId;
+
+  constructor(private chatService: ChatService) {}
+
+  ngOnInit() {
+    this.chatService.sendMessage(this.userId);
+
+    this.chatService.getNewMessage().subscribe((userId: string) => {
+      const message = this.userId === userId ? 
+        `=> You joined the session, welcome! %c (id: ${userId})` : 
+        `=> New user has joined the session! %c (id: ${userId})`;
+      console.log(
+        `%c ${message}`, 
+        'background: #90ee90; color: white; font-size: 16px', 
+        'background: #90ee90; color: black; font-size: 16px'
+      );
+    });
+  }
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/chat.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  constructor(private socket: Socket) {
+    // this.socket.on('connection', () => {
+
+    // });
+  }
+
+  sendMessage(message: string): void {
+    this.socket.emit('message', message);
+  }
+
+  getNewMessage(): Observable<string> {
+    return this.socket.fromEvent('newMessage');
+  }
+}
